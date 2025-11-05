3ffca95dfc66e5430df1fc8d1a4a1d387c1bbe53
Sameer-Sharma-SAM <www.sameersharma12@gmail.com>
2025-11-05 21:33:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-function getFormvalue() {
-    //Write your code here
+    function getFormvalue(e) {
+      e.preventDefault();
 
-}
+      let first_name = document.forms["form1"]["fname"].value.trim();
+      let last_name = document.forms["form1"]["lname"].value.trim();
+
+      if (!first_name && !last_name) {
+        alert("Please enter your first and last name");
+        return;
+      }
+
+      alert(`${first_name} ${last_name}`.trim());
+    }
